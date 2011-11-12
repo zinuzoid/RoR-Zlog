8fae101d0f33267ae25602b3832e8657a124c38f
Jetsada Machom <zinuzoid@gmail.com>
2011-11-12 21:22:27 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zlog::Application.config.secret_token = '86d9a710d3aa347493f1c78bdfff0c049391457fbb92d03e82483eb1d1e63f6f7e7ee75a83069d974dc3cd44ad3595d562fba545bf6c9ad255f72cffe02e2ec2'
